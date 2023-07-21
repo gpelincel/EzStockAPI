a566c32f6fa5f0cbf91facfddd7e0d4da279cbd8
Gabriel Pelincel <gabrielpelincel@gmail.com>
2023-07-21 14:37:06 -0300

    feat: Brand seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProductSeeder::class,
+            BrandSeeder::class,
             SupplierSeeder::class,
         ]);
     }
